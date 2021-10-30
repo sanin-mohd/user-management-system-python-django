f5bf498401f7d7a6c867cb3703ade9a941a4f224
sanin-mohd <sanin.nitcalicut.ece@gmail.com>
2021-10-30 12:51:55 +0530

    d

--- testapp/settings.py
+++ testapp/settings.py
@@ -83,6 +83,25 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+
+#     'default': {
+
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+#         'NAME': 'week6',
+
+#         'USER': 'postgres',
+
+#         'PASSWORD': '1234',
+
+#         'HOST': 'locahost',
+
+#         'PORT': '5432',
+
+#     }
+
+# }
 
 
 # Password validation
